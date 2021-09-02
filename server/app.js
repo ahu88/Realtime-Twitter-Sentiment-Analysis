0b486d2f8d3fd1c24328e4c22ab4e3b84920509c
ahu88 <53159426+ahu88@users.noreply.github.com>
2021-09-02 13:57:17 -0400

    added user input for tweet keywords

--- server/app.js
+++ server/app.js
@@ -1,19 +1,43 @@
+//add socket.io -> allows use of web sockets, open door connection between server and client (basic html + javascript, React (build folder?), Angular ...)
 const http = require('http') 
-const express = require('express') 
+const express = require('express')
 const socketIO = require('socket.io')
+//const cors = require('cors');
 
-const needle = require('needle') 
-const config = require('dotenv').config() 
+const needle = require('needle') //http client to make our requests
+const config = require('dotenv').config() //to use .env file - to get token
 const TOKEN = process.env.TWITTER_BEARER_TOKEN
 const PORT = process.env.PORT || 4001
 const index = require("./routes/index");
 
-const app = express() 
-app.use(index) 
+const app = express() //initialize express
 
+//to extract information from POST requests
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded({extended:true})); // to support URL-encoded bodies
+
+app.use(index) //points to routes defined in index.js
+
+//to allow front end to make api calls to back end
+//app.use(cors());
+// app.use(
+//     cors({
+//       origin: ["http://localhost:3000"],
+//       credentials: true,
+//     })
+//   );
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+
+//set up server + sockets
 const server = http.createServer(app) //want to listen to this server
+
 //FOR USING REACT CLIENT
-const io = socketIO(server, {
+var io = socketIO(server, {
     cors: {
         origin: "http://localhost:3000", //client endpoint
         methods: ["GET", "POST"],
@@ -21,98 +45,156 @@ const io = socketIO(server, {
         credentials: true
     }
 })
-//const io = socketIO(server) 
 
-const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id' 
+/* store a reference to the io object so that it can be used in routes !!! */
+app.set('socketio', io);
 
-const rules = [{value: 'anime'}]
 
-//1) get stream rules
-async function getRules() {
-    const response = await needle('get', rulesURL, {
-        headers: {
-            Authorization: `Bearer ${TOKEN}`
-        }
-    })
-    return response.body
-}
 
-//2) set stream rules
-async function setRules() {
-    const data = { 
-        add: rules
-    }
 
-    const response = await needle('post', rulesURL, data, {
-        headers: {
-            'content-type' : 'application/json',
-            Authorization: `Bearer ${TOKEN}`
-        }
-    })
 
-    return response.body
-}
+// const rules = [{value: 'demon slayer'}]
 
-//3) delete stream rules
-async function deleteRules(rules) {
-    if (!Array.isArray(rules.data)){
-        return null
-    }
+// const ruleFunctions = require('./strategies/ruleFunctions');
 
-    const ids = rules.data.map((rule) => rule.id)
+// io.on('connection', async () => {
+//     console.log('Client connected')
+//     let currentRules
+    
+//     // socket_id.push(io.id);
+//     // if (socket_id[0] === io.id) {
+//     //     // remove the connection listener for any subsequent 
+//     //     // connections with the same ID
+//     //     io.removeAllListeners('connection'); 
+//     // }
 
-    const data = { 
-        delete: {
-           ids: ids 
-        }
-    }
+//     try {
+//         //get all stream rules
+//         currentRules = await ruleFunctions.getRules()
 
-    const response = await needle('post', rulesURL, data, {
-        headers: {
-            'content-type' : 'application/json',
-            Authorization: `Bearer ${TOKEN}`
-        }
-    })
+//         //delete all stream rules
+//         await ruleFunctions.deleteRules(currentRules)
 
-    return response.body
-}
+//         //set rules base on rules array
+//         await ruleFunctions.setRules(rules)
+    
+//     } catch (err) {
+//         console.error(err)
+//         process.exit(1)
+//     }
 
-function streamTweets(socket) {
-    const stream = needle.get(streamURL, {
-        headers: {
-            Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAAPtGPAEAAAAA1TnVkUnuELQcbZpqUt%2F5j7ZdoCE%3D8A59EuclpznwaOqAYWWtggq3t9H0rEtDIfS6I5RnAxT4766drb`
-        }
-    })
+//     //pass in socket 
+//     ruleFunctions.streamTweets(io)
 
-    stream.on('data', (data) => {
-        try {
-            const json = JSON.parse(data)
-            socket.emit('tweet', json)
-        } catch(error) {
-            //console.log(error)
-            //console.log(data)
-        }
-    })
-}
+// })
 
-//add event handler for socket io -> runs when client connects
-io.on('connection', async () => {
-    console.log('Client connected')
-    let currentRules
 
-    try {
-        currentRules = await getRules()
-        await deleteRules(currentRules)
-        await setRules()
-    } catch (err) {
-        console.error(err)
-        process.exit(1)
-    }
 
-    //pass in socket 
-    streamTweets(io)
-})
+// const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+// const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id' //by default tweets return id and text, can add query to add more, such as retweets + likes
+
+// const rules = [{value: 'demon slayer'}]
+
+// //3 separate functions for the rules:
+// //1) get stream rules -> using needle + PROMISES
+// async function getRules() {
+//     const response = await needle('get', rulesURL, { //specified in needle documentation
+//         headers: {
+//             Authorization: `Bearer ${TOKEN}`
+//         }
+//     })
+//     //console.log(response)
+//     return response.body
+// }
+
+// //2) set stream rules
+// async function setRules() {
+//     const data = { //specified in the twitter api documentation
+//         add: rules
+//     }
+
+//     const response = await needle('post', rulesURL, data, {
+//         headers: {
+//             'content-type' : 'application/json',
+//             Authorization: `Bearer ${TOKEN}`
+//         }
+//     })
+
+//     return response.body
+// }
+
+// //3) delete stream rules
+// async function deleteRules(rules) {
+//     if (!Array.isArray(rules.data)){ //rules.data needs to be an array!
+//         return null
+//     }
+
+//     const ids = rules.data.map((rule) => rule.id)
+
+//     const data = { 
+//         delete: {
+//            ids: ids 
+//         }
+//     }
+
+//     const response = await needle('post', rulesURL, data, {
+//         headers: {
+//             'content-type' : 'application/json',
+//             Authorization: `Bearer ${TOKEN}`
+//         }
+//     })
+
+//     return response.body
+// }
+
+// function streamTweets(io) {
+//     //make request to stream url -> get stream
+//     const stream = needle.get(streamURL, {
+//         headers: {
+//             Authorization: `Bearer ${TOKEN}`
+//         }
+//     })
+
+//     //Attach event handlers on the stream https://www.w3schools.com/jquery/event_on.asp, 'data' is the event, (data) is the function to run when the event occurs (i.e. gives us the data)
+//     //data is a buffer -> need to parse to json
+//     stream.on('data', (data) => {
+//         try {
+//             const json = JSON.parse(data)
+            
+//             //emit an event called tweet, how to communicate between client and backend -> send an event ('tweet') to client, and pass data along (tweets)
+//             io.emit('tweet', json)
+//             //console.log(json)
+//         } catch(error) {
+//             //console.log(error)
+//             console.log(data)
+//         }
+//     })
+// }
+
+// //add event handler for socket io -> runs when client connects
+// io.on('connection', async () => {
+//     console.log('Client connected')
+//     let currentRules
+
+//     try {
+//         //get all stream rules
+//         currentRules = await getRules()
+
+//         //delete all stream rules
+//         await deleteRules(currentRules)
+
+//         //set rules base on rules array
+//         await setRules()
+    
+//     } catch (err) {
+//         console.error(err)
+//         process.exit(1)
+//     }
+
+//     //pass in socket 
+//     streamTweets(io)
+
+// })
 
 io.on('disconnect', () =>{
     console.log('Client DISCONNECTED')
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,62 @@
+// Use express's router to route all our API endpoints
 const path = require('path');
 const express = require("express");
 const router = express.Router();
+const ruleFunctions = require('../strategies/ruleFunctions');
+const socketIO = require('socket.io');
 
-router.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../', 'client', 'index.html'))
-    //res.send({ response: "I am alive" }).status(200);
-})
+//*** MAYBE ISSUE WITH ASYNC FOR HANDLING POST REQUESTS - USE PROMISES INSTEAD??? https://stackoverflow.com/questions/56119131/node-js-and-express-wait-for-asynchronous-operation-before-responding-to-http-r */
+// POST Request - set the rules array, stream tweets
+router.post("/",  async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    console.log("POST MALONE");
+
+    var rulesArray = [];
+    var rules = req.body.rules;
+    var rulesJson = {value: `${rules}`}
+
+    rulesArray.push(rulesJson);
+
+    let socket_id = [];
+    var io = req.app.get('socketio');
+
+    console.log(`Rules: ${rules}`);
+    console.log(`RulesArray: ${rulesArray}`);
+
+    //add event handler for socket io -> runs when client connects
+    //io.on('connection', async () => {
+        console.log('Client connected')
+        let currentRules
+        
+        // socket_id.push(io.id);
+        // if (socket_id[0] === io.id) {
+        //     // remove the connection listener for any subsequent 
+        //     // connections with the same ID
+        //     io.removeAllListeners('connection'); 
+        // }
+
+        try {
+            //get all stream rules
+            currentRules = await ruleFunctions.getRules()
+
+            //delete all stream rules
+            await ruleFunctions.deleteRules(currentRules)
+
+            //set rules base on rules array
+            await ruleFunctions.setRules(rulesArray)
+        
+        } catch (err) {
+            console.error(err)
+            process.exit(1)
+        }
+
+        //pass in socket 
+        ruleFunctions.streamTweets(io)
+
+    //})
+
+});
+
 
 module.exports = router;
